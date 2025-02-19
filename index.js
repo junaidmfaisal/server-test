8ecc625721e9207c66811e1e7d39a02a7fd39b1d
junaidmfaisal <junaidmfaisal@gmail.com>
2025-02-19 16:32:12 +0530

    v1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./database/db.js');
+require('./database/db.js');
 
 const app = express();
 app.use(express.json());
